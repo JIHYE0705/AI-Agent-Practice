40c5c70a709ce3d329eb51b3e8e3adfd5a338800
JIHYE0705 <jihyem75@gmail.com>
2026-01-24 14:02:24 +0900

    03-2 multiturn

---
+++ chapter3/sec02/single_turn.py
@@ -0,0 +1,21 @@
+from ollama_client import OllamaClient
+
+ollama = OllamaClient()
+if __name__ == '__main__':
+    while True:
+        user_input = input('사용자: ')
+
+        if user_input == 'exit':
+            break
+
+        response = ollama.chat(
+            messages=[
+                {
+                    'role': 'system', 'content': '너는 사용자를 도와주는 상담사야'
+                },
+                {
+                    'role': 'user', 'content': user_input
+                }
+            ]
+        )
+        print('AI: ' + response['message']['content'])
